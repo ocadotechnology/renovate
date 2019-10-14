58fe6a3e2327e89e5ea7e0fdd79f509216442741
v.ivanov <v.ivanov@ocado.com>
2019-10-14 10:11:13 +0300

    Support GitLab-hosted changelogs #3323

--- lib/workers/pr/changelog/index.ts
+++ lib/workers/pr/changelog/index.ts
@@ -1,28 +1,53 @@
+<<<<<<< HEAD:lib/workers/pr/changelog/index.ts
 import { logger } from '../../../logger';
 import * as versioning from '../../../versioning';
 import * as sourceGithub from './source-github';
+import * as sourceGitlab from './source-gitlab';
 import { getReleases } from './releases';
 import { ChangeLogConfig, ChangeLogResult } from './common';
-
+import { util } from 'util';
+import { URL } from 'url';
 export * from './common';
 
 export async function getChangeLogJSON(
   args: ChangeLogConfig
 ): Promise<ChangeLogResult | null> {
-  const { sourceUrl, versionScheme, fromVersion, toVersion } = args;
-  if (!sourceUrl) {
-    return null;
-  }
+  logger.debug(
+    `get platform name as ${args.platform} in function getChangeLogJSON`
+  );
+  logger.debug(`args for getChangeLogJSON`);
+  logger.debug(util.inspect(args, { showHidden: false, depth: null }));
+  const { sourceUrl, versionScheme, fromVersion, toVersion, homepage } = args;
   const version = versioning.get(versionScheme);
+  // let { sourceUrl, versionScheme, fromVersion, toVersion, homepage } = args;
+  // if (!sourceUrl) {
+  if (!sourceUrl && !homepage) {
+    logger.debug(
+      `no sourceUrl or homepage provided for ${args.depName}, can't provide release notes!`
+    );
+    return null;
+  } else {
+    if (!sourceUrl) {
+      let tmpSourceUrl = URL.parse(args.homepage);
+      args.sourceUrl = `${tmpSourceUrl.protocol}//${tmpSourceUrl.hostname}/${tmpSourceUrl.pathname}`;
+    }
+  }
   if (!fromVersion || version.equals(fromVersion, toVersion)) {
     return null;
   }
-
   const releases = args.releases || (await getReleases(args));
 
   try {
-    const res = await sourceGithub.getChangeLogJSON({ ...args, releases });
-    return res;
+    if (args.sourceUrl.includes('gitlab')) {
+      logger.debug(
+        `Running changelog creation for gitlab with parameters ${JSON.stringify(args)}`
+      );
+      const res = await sourceGitlab.getChangeLogJSON({ ...args, releases });
+      return res;
+    } else {
+      const res = await sourceGithub.getChangeLogJSON({ ...args, releases });
+      return res;
+    }
   } catch (err) /* istanbul ignore next */ {
     logger.error({ err }, 'getChangeLogJSON error');
     return null;
